fea8b00227498f88d14e2e5aedd87d34097334a9
NikitaKorde <nikikorde31@gmail.com>
2020-01-20 13:37:03 +0530

    Hello NodeJS Application

---
+++ config.js
@@ -0,0 +1,15 @@
+var config = {
+	database: {
+		host:	  'localhost', 	// database host
+		user: 	  'root', 		// your database username
+		password: '', 		// your database password
+		port: 	  3308, 		// default MySQL port
+		db: 	  'demo' 		// your database name
+	},
+	server: {
+		host: '127.0.0.1',
+		port: '3000'
+	}
+}
+
+module.exports = config
